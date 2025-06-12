6002c87272c535f2381b1eb37baa61517a4158a2
sobhan9012 <sobhansahoo2000@gmail.com>
2025-06-12 16:21:00 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv'
+import connectDB from "../src/db/index.js";
+
+import { app } from "./app.js"
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+    .then(() => {
+
+        app.on("error", (error) => {
+            console.log("ERROR", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`⚙️  Server running on port ${process.env.PORT} 🔥`)
+        });
+    })
+    .catch((error) => {
+        console.log("MONGODB CONNECTION FAILED");
+        console.log("ERROR", error)
+        throw error
+    })
